9f27c504f706a49ee297d07bb579c0bc1273c35a
samashtar <sam.ashtar@flatironschool.com>
2018-10-22 13:29:39 -0400

    full crud fetches work

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,5 +8,26 @@ class NotesController < ApplicationController
     render json: @note
   end
 
+  def create
+    @note = Note.create(note_params)
+    render json: @note
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @note.update(note_params)
+    render json: @note
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+  end
+
+private
+
+def note_params
+  params.require(:note).permit(:name, :description, :day_id)
+end
 
 end
